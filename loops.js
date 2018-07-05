4056e295d58adf288aaed6b607c6926a92188534
katy@karstaway.com <katy@karstaway.com>
2018-07-05 07:35:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,5 +18,11 @@ function whileLoop (number) {
 }
 
 function doWhileLoop (array) {
-  
+  return Math.random() >= 0.5
 }
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')} 
+  while (array.length > 0 && maybeTrue;
+}
+return array
